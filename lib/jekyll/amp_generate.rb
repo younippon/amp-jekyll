c415a4924808d114aa00ec7092c71786f86aca27
younippon <hikonyan.mh36@gmail.com>
2017-02-26 12:41:27 +0900

    Add related_posts to amp site

--- lib/jekyll/amp_generate.rb
+++ lib/jekyll/amp_generate.rb
@@ -9,6 +9,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'amp.html')
       self.content               = post.content
+      self.related_post          = post.related_post
       self.data['body']          = (Liquid::Template.parse post.content).render site.site_payload
 
       # Merge all data from post so that keys from self.data have higher priority
